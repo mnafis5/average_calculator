ec5480afc896193a8c74251ef6dc6b0923657b60
Muchammad Nafiz Dhiaulhaq <94342046+mnafis5@users.noreply.github.com>
2023-01-31 09:51:45 +0700

    Fixing bug and new features
    Fixing bug and adding new features such as pytagoras calculator. And Turned the script into OOP.

--- Cs_project/Program.cs
+++ Cs_project/Program.cs
@@ -1,15 +1,15 @@
-﻿//Make a CLI application that outputs the average of data or numbers
-
-//when running the application, the first content that displays on the screen should be an opening text.
-//enter the numbers!
-//the output
-//below the output, there is an alert warning about the continuation from the user
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+//Make a CLI application that outputs the average of data or numbers
+
+//when running the application, the first content that displays on the screen should be an opening text.
+//enter the numbers!
+//the output
+//below the output, there is an alert warning about the continuation from the user
 
 namespace Cs_project
 {
@@ -17,52 +17,106 @@ namespace Cs_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO THE AVE APP!");
-            Console.WriteLine("***********************");
-            Console.WriteLine("This is an app for calculating the average in a collection of numbers. Made by MNafisdh.");
-
-            Console.Write("Input some numbers here:");
-            //handling the input
-            Calculate();
-
-            Console.Write("Do you want to continue(y/n)...");
-            char ctn = Convert.ToChar(Console.ReadLine());
-            string[,] stroke = { {"|_"}, {"_|"} };
-            if (ctn == 'y')
+            while (true)
             {
-                Console.Write("Input some numbers here:");
-                Calculate();
-            }else
+             Console.WriteLine("WELCOME TO THE AVE APP!");
+             Console.WriteLine("***********************");
+             Console.WriteLine("This is an app for calculating a collection of numbers. Made by MNafisdh.");
+
+             Console.WriteLine("1. Average Calculation");
+             Console.WriteLine("2. Pytagoras Calculation");
+             Console.Write("Guest>>");
+             int choice = Convert.ToInt32(Console.ReadLine());
+             //handling the input
+            if(choice == 1)
             {
-                Console.WriteLine("Bye!");
-               foreach (string item in stroke)
-               {
-                  Console.Write(item);
-                  Console.Write(item);
-                  Console.WriteLine(item);
-                  Console.Write(item);
-                  Console.Write(item);
-               }
+             string av_inp = Console.ReadLine();
+             Calculation average = new Average();
+             average.Calculate(av_inp);
+            }else if(choice == 2)
+            {
+             Console.WriteLine("1. Determine the longest line");
+             Console.WriteLine("2. Determine the normal line");
+             int py_inp = Convert.ToInt32(Console.ReadLine());
+             if(py_inp == 1)
+             {
+                Calculation pytagoras = new Pytagoras();
+                string inp_py = Console.ReadLine();
+                pytagoras.Calculate(inp_py);
+             }else{
+                Pytagoras pytagoras1 = new Pytagoras();
+                string inp_py = Console.ReadLine();
+                pytagoras1.Reverse_Calculate(inp_py);
+             }
+
+            }else{
+                Console.WriteLine("input invalid");
+            }
+             Console.Write("Do you want to continue(y/n)...");
+             string conss = Console.ReadLine();
+             if (conss == "n")
+             {
+                break;
+             }
             }
 
         }
-        static void Calculate()
+        class Calculation
         {
-             string input = Console.ReadLine();
-            //split the coma and convert to char
-            string[] slice = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            
-            //convert to int and array
-            double[] numbers = slice.Select(x => Convert.ToDouble(x)).ToArray();
-
-            //output
-            string output = "The average is:";
-            double num = numbers.Sum() / numbers.Length;
-            Console.WriteLine(output + " " + num);
-            Console.WriteLine($"Sum: {numbers.Sum()}");
-            Console.WriteLine($"Count: {numbers.Length}");
-            Console.WriteLine($"Largest: {numbers.Max()}");
-            Console.WriteLine($"Smallest: {numbers.Min()}");
+            public virtual void Calculate(string input)
+            {
+                Console.WriteLine(input);
+            }
         }
+         class Average : Calculation
+         {
+            public override void Calculate(string input)
+            {
+             //split the coma and convert to char
+             string[] slice = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            
+             //convert to int and array
+             double[] numbers = slice.Select(x => Convert.ToDouble(x)).ToArray();
+
+             //output
+             double num = numbers.Sum() / numbers.Length;
+             Console.WriteLine("The average is" + "  " + num);
+             Console.WriteLine($"Sum: {numbers.Sum()}");
+             Console.WriteLine($"Count: {numbers.Length}");
+             Console.WriteLine($"Largest: {numbers.Max()}");
+             Console.WriteLine($"Smallest: {numbers.Min()}");
+            }
+         }
+
+         class Pytagoras : Calculation
+         {
+            public override void Calculate(string input)
+            {
+             string[] slice = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            
+             //convert to int and array
+             double[] numbers = slice.Select(x => Convert.ToDouble(x)).ToArray();
+
+             //output
+             double num = Math.Pow(numbers[0],2) + Math.Pow(numbers[1],2);
+             double res = Math.Sqrt(num);
+
+             Console.WriteLine(res + " cm");
+            }
+
+            public void Reverse_Calculate(string input)
+            {
+             string[] slice = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            
+             //convert to int and array
+             double[] numbers = slice.Select(x => Convert.ToDouble(x)).ToArray();
+
+             //output
+             double num = Math.Pow(numbers[0],2) - Math.Pow(numbers[1],2);
+             double res = Math.Sqrt(num);
+
+             Console.WriteLine(res + " cm");
+            }
+         }
     }
 }
